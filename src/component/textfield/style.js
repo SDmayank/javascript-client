238a323f8d3257d49494128928008b3367cbff3b
SDmayank <mayank.garg@successive.tech>
2020-03-11 11:29:17 +0530

    feat:Input controllers(#40535)

--- src/component/textfield/style.js
+++ src/component/textfield/style.js
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 
 const Input = styled.input`
 width: 100%;
+padding: 12px 20px;
+display: inline-block;
+border: 1px solid #ccc;
+border-radius: 4px;
 `;
 
 export default Input;
