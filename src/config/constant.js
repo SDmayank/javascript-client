6ce66ea639af31305bf0ca915296274253218d27
SDmayank <mayank.garg@successive.tech>
2020-03-19 15:29:49 +0530

    revamp:Code Revamp (feat/#40835)

--- src/config/constant.js
+++ src/config/constant.js
@@ -59,7 +59,7 @@ const radioOptionsFootball = [
 ];
 const schema = yup.object().shape({
   name: yup.string().required('Please enter your Name').min(3, 'Please enter no less than 3 characters'),
-  email: yup.string().required('Please select an emailAddress').matches(/([a-zA-Z0-9_\-.]+)@successive[.]tech$/gmi, 'please enter vaild email'),
+  email: yup.string().required('Please select an emailAddress').matches(/([a-zA-Z0-9_\-.]+)@gmail[.]com$/gmi, 'please enter vaild email'),
   password: yup.string().min(8, 'Password must be of 8 characters')
     .required('Password is required'),
   confirmPassword: yup.string()
