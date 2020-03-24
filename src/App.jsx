ca36bdec60324c162e4a366d864a83774bb9fa93
SDmayank <mayank.garg@successive.tech>
2020-03-24 15:36:14 +0530

    feat:Routes setup (feat/#40989)

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,6 @@ import { PrivateRoute, AuthRoute } from './routes/index';
 import {
   TextfieldDemo, InputDemo, ChildrenDemo, Trainee, NotFound, Login,
 } from './Pages';
-// import { ThemeProvider } from '@material-ui/core/styles';
-// import { Typography } from '@material-ui/core';
-// import { theme } from './theme';
-// import { Trainee } from './pages/index';
-// import Login from './pages/Login/index';
-
 function App() {
   return (
     <Router>
