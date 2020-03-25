e424e2433b5a4dd2d3082cfda32055d521db897d
SDmayank <mayank.garg@successive.tech>
2020-03-25 13:38:33 +0530

    feat: Dynamic routing(feat/#41016)

--- src/Pages/Trainee/TraineeList.jsx
+++ src/Pages/Trainee/TraineeList.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-array-index-key */
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
 import { AddDialog } from './component';
 import trainees from './data/trainee';
 
+const useStyles = (theme) => ({
+  button: {
+    marginTop: theme.spacing(2),
+    align: 'Right',
 
-class Trainee extends React.Component {
+  },
+  buttonPosition: {
+    display: 'flex',
+  },
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+});
+
+class Trainee extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -13,42 +42,56 @@ class Trainee extends React.Component {
     };
   }
 
-  handleClickOpen = () => {
-    this.setState({ open: true }, () => { console.log(this.state); });
+  onOpen = () => {
+    let { open } = this.state;
+    open = true;
+    this.setState({ open });
   };
 
-  handleClose = () => {
-    this.setState({ open: false }, () => { console.log(this.state); });
+  onClose = () => {
+    let { open } = this.state;
+    open = false;
+    this.setState({ open });
   };
 
-  handleSubmit = (data) => {
-    this.setState({ open: false }, console.log(data));
-  };
+  onSubmit = () => {
+    const { open } = this.state;
+    this.setState({ open: false }, () => {
+    });
+    return open;
+  }
 
   render() {
     const { open } = this.state;
-    // const { trainees } = props;
-    // console.log("/?????", trainees[0].name)
+    const { match: { url } } = this.props;
+    const { classes } = this.props;
+
     return (
-      <>
-        <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
-          ADD TRAINEE
-        </Button>
-        <AddDialog open={open} onClose={this.handleClose} onSubmit={() => this.handleSubmit} />
+      <div className={classes.paper}>
+        <div className={classes.buttonPosition}>
+          <Button variant="outlined" color="primary" onClick={this.onOpen} className={classes.button}>
+            ADD TRAINEE
+          </Button>
+        </div>
+        <AddDialog open={open} onClose={this.onClose} onSubmit={() => this.onSubmit} />
         <ul>
           {
             trainees && trainees.length && trainees.map((trainee) => (
-              <li>
-                <Link to={`/Trainee/${trainee.id}`}>
-                  {trainee.name}
-                </Link>
-              </li>
+              <Fragment key={trainee.id}>
+                <li>
+                  <Link to={`${url}/${trainee.id}`}>{trainee.name}</Link>
+                </li>
+              </Fragment>
             ))
           }
-
         </ul>
-      </>
+      </div>
     );
   }
 }
-export default Trainee;
+
+Trainee.propTypes = {
+  classes: PropTypes.objectOf(PropTypes.string).isRequired,
+};
+
+export default withStyles(useStyles)(Trainee);
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,11 +1,11 @@
 import ChildrenDemo from './childrenDemo';
 import InputDemo from './InputDemo';
 import TextfieldDemo from './TextfieldDemo';
-import Trainee from './Trainee';
+import TraineeRoutes from './Trainee';
 import Login from './Login';
 import NavBar from './components';
 import NotFound from './nomatch';
 
 export {
-  InputDemo, TextfieldDemo, Trainee, NavBar, Login, NotFound, ChildrenDemo,
+  InputDemo, TextfieldDemo, TraineeRoutes, NavBar, Login, NotFound, ChildrenDemo,
 };
