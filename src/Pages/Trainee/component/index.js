d930ac12375fa85c94d6ab5c821aa64b6635242b
SDmayank <mayank.garg@successive.tech>
2020-03-25 16:07:31 +0530

    feat:React Table Component(feat/#41046)

--- src/Pages/Trainee/component/index.js
+++ src/Pages/Trainee/component/index.js
@@ -1,5 +1,6 @@
 import validKey from './DialogComponents';
 import AddDialog from './AddDialog';
+import TraineeTable from './Table/table';
 
 export default validKey;
-export { AddDialog };
+export { AddDialog, TraineeTable };
