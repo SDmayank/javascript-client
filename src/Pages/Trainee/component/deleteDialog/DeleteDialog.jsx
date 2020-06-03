ec39185f658c46bc16764e25b43bf6959f0e9f95
SDmayank <mayank.garg@successive.tech>
2020-06-03 13:00:30 +0530

    feat:Pagination on table & fix issue in sorting(#41191)

--- src/Pages/Trainee/component/deleteDialog/DeleteDialog.jsx
+++ src/Pages/Trainee/component/deleteDialog/DeleteDialog.jsx
@@ -4,7 +4,7 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 
 export default class DeleteOpenDialog extends Component {
   constructor(props) {
@@ -54,8 +54,8 @@ export default class DeleteOpenDialog extends Component {
 }
 
 DeleteOpenDialog.propTypes = {
-  open: PropTypes.bool.isRequired,
-  onClose: PropTypes.func.isRequired,
-  onSubmit: PropTypes.func.isRequired,
-  data: PropTypes.objectOf(PropTypes.string).isRequired,
+  open: propTypes.bool.isRequired,
+  onClose: propTypes.func.isRequired,
+  onSubmit: propTypes.func.isRequired,
+  data: propTypes.objectOf(propTypes.string).isRequired,
 };
